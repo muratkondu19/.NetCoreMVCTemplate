043a1e1b2b8c9ca954a51eb56396493e861cdc7c
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-05 07:20:54 +0300

    Controller yapısı oluşturuldu.

---
+++ OrnekNetCoreUyg2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrnekNetCoreUyg2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Bir class'ın request karşılayabilmesi için Controller sınıfından kalıtım alması gerekmektedir. 
+        //Controller -> bir sınıfı request alabilir ve response döndürebilir / controller yapabilmek için ilgili sınıfı bu class'tan türetmek gerekir. 
+
+        //Controller içerisindeki istekleri karşılayan metotlara action metod denir. 
+        //Controller sınıfı içerisindeki tüm metotlar action metotdur. 
+        //Action metot->controllera gelen isteği karşılayan ve gerekli operasyonları gerçekleştiren metotlardır. 
+        public IActionResult Index()
+        {
+            return View();
+        }
+   
+    }
+}
---
+++ OrnekNetCoreUyg2/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrnekNetCoreUyg2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult UrunleriGetir()
+        {
+            return View();
+        }
+    }
+}
