a4b55e5f965ba7fb9dca8fae2db3ae604b47ca4f
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-05 16:13:28 +0300

    Custom HtmlHelper Fonkisyonu oluşturma

---
+++ OrnekNetCoreUyg2/Extensions/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OrnekNetCoreUyg2.Extensions
+{
+    static public class Extensions
+    {
+        //Herhangi bir yapı ya da değerin custom halini extensionlar ile oluşturabiliriz. 
+        //CustomTextBox fonksiyonu mimarideki IHtmlHelper türleirne extension olarak eklenmiştir. 
+        public static IHtmlContent CustomTextBox(this IHtmlHelper htmlHelper, string name, string value="inpıt area",string placeholder=null)
+        {
+            //new alanındaki değerler parametre olarak alınabilir ya da bir dto ile karşılanabilir. 
+            return htmlHelper.TextBox(name, value, new { style = "background-color:green,color:white,font-size:11px;", @class = "form-input",placeholder=placeholder });
+        }
+    }
+}
