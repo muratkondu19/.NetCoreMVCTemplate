54cb6260d449a58122cdd81afbd5cd0e27d05b60
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-05 07:54:17 +0300

    View yapısı oluşturuldu.

--- OrnekNetCoreUyg2/Controllers/ProductController.cs
+++ OrnekNetCoreUyg2/Controllers/ProductController.cs
@@ -4,9 +4,25 @@ namespace OrnekNetCoreUyg2.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult UrunleriGetir()
+        public IActionResult GetProducts()
         {
-            return View();
+            //Bir controllera ait viewların hepsi ilgili controller ismi altında bulunması gerekiyor. Views/Product
+            //Default olarak ilgili actiona ait bir view oluşturulacaksa, action ile aynı isimde olması gerekmektedir. 
+            //View fonksiyonu bu actiona ait cshtml dosyasını tetikleyecek olan fonksiyondur. 
+
+            //return View(); //->ilgili action ismiyle birebir aynı olan viewı tetikler
+
+
+            // belirtilen view ismindeki view dosyasını render eder. 
+            ViewResult result = new ViewResult();
+            result = View("other");
+            return result;
+
+
+
+
+
+
         }
     }
 }
